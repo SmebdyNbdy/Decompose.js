27809282862861d87b40310b5a2fe65594dee5ec
Alexander Gavrikov <alexander@gavrikov.me>
2021-06-11 12:38:41 +0300

    purged old code from /src/classes/Style.js

--- src/decompose/classes/Style.js
+++ src/decompose/classes/Style.js
@@ -4,13 +4,8 @@ export class Style {
 
     static get regex() {
         return {
-            //atNest: /(@nest)([^{\n]+{[^}]+})/g,
-            //oldNesting: /(^|;|})\s*([\w\-.#:[="\]~>+ ,*]*[\w\-\]*])(?:\s*{)((?:\s*(?:[\w-]+):[^;]+;)*)(?:\s*)(\s*[&\w\-.#:[="\]~>+ ,*]*[\w\-\]*])(?:\s*{)((?:(?:\s*(?:[\w-]+):[^;]+;)|(?:\s*&[^{}]+{[\s\S]*?}))*\s*})/,
             nesting: /(\s*§)([^{@§}]+)(§\s*{\s*)((?:[-\w]+:[^;]+;\s*)*)(§)([^{§}]+)(§\s*{\s*(?:[-\w]+:[^;]+;\s*)*})/,
-            //queryCleanup: /@[\w\-:( )%&]+\s*{\s*}/g,
             cleanup: /\s*§[^{§}]+§\s*{\s*}/g,
-            //nestCleanup: /(@nest\s?{\s?)(?:(?:|[^{}]|{[^{}]*})+)(\s})/g,
-            //oldQueries: /([\w\-.#:[="\]~>+ ,*]*[\w\-\]*])(?:\s*{)((?:(?:[^{@}]*)|(?:[^{@}]*{[\s\S]*?}[^{@}]*))*)(\s*@[\w\-:( )%&]+\s*{\s*)(?:\s*)(\s*[&\w\-.#:[="\]~>+ ,*]*[\w\-\]*])(?:\s*{)((?:(?:\s*(?:[\w-]+):[^;]+;)|(?:\s*&[^&{}]+{[\s\S]*?}))*\s*})\s*}/,
             queries: /(§[^{§@}]+§)(\s*{.*[^{§@}]*)(§@[^§@]+§)(\s*{)((?:\s*§[^{§}]*§\s*{(?:\s*[\w-]+:[^;]*;)*\s*})*)(\s*})/,
             markSelectors: /(^|;|{|})(?:\s*)([\w\-#.[\]="~>+:@&]+[@&\w\-#.,[\]="~>+ :]*[&\w\]*])(?:\s*)(?={)/g,
             unmarkSelectors: /§([^{§}]+)§/g,
@@ -25,38 +20,6 @@ export class Style {
                 .replaceAll(/(\s*)(\{|\}|;)(\s*)/g, "$2 ")
                 .replaceAll("@nest", "")
                 .replaceAll(Style.regex.markSelectors, "$1 §$2§");
-        }
-    }
-
-    static get oldReplaceNesting() {
-        return (_, $1, $2, $3, $4, $5) => {
-            let replacement = $2.split(",");
-            let target = $4.split(",");
-            let result = [];
-            for (let e in replacement) {
-                let sel = replacement[e];
-                for (let f in target) {
-                    result.push(target[f].replace("&", sel));
-                }
-            }
-            return `${$1}${result.join(",")} {${$5}${$2} {${$3}`;
-        }
-    }
-
-    static get oldReplaceQueries() {
-        return (_, $1, $2, $3, $4, $5) => {
-            let replacement = $1.split(",");
-            let target = $4.split(",");
-            let result = [];
-            for (let e in replacement) {
-                let sel = replacement[e];
-                for (let f in target) {
-                    if (target[f].replace("&", sel) !== result[result.length-1]) {
-                        result.push(target[f].replace("&", sel));
-                    }
-                }
-            }
-            return `${$3}${result.join(",")} {${$5}} ${$1} {${$2}`;
         }
     }
 
@@ -97,7 +60,6 @@ export class Style {
 
     insert(rule) { //мы преобритем ботиночьки!!!!!!!
         let output = Style.normalize(rule);
-        let t0 = Date.now();
 
         let cyclicApply = (regex, replacer) => {
             while (output.match(regex)) {
@@ -107,31 +69,23 @@ export class Style {
             return output;
         };
         cyclicApply(Style.regex.nesting, Style.replaceNesting);
-        console.log(output);
         if (output.includes("@")) {
             cyclicApply(Style.regex.queries, Style.replaceQueries);
-            console.log(output);
             cyclicApply(Style.regex.nesting, Style.replaceNesting);
-            console.log(output);
         }
         output = output.replaceAll(Style.regex.unmarkSelectors, "$1");
         console.log(output);
-        cyclicApply(Style.regex.separate, this.insertRule);
-        console.log(output);
 
-        let t1 = Date.now();
-        console.log(`stylesheet build time is ${(t1-t0)/100} s`)
+        cyclicApply(Style.regex.separate, this.insertRule);
         return this;
     }
 
     insertScoped(scope, rule) {
         let scopedRule = rule.replaceAll(/\s+/g, " ");
-        console.log(scopedRule);//Style.normalize(rule);
         while (scopedRule.match(Style.regex.scope)) {
             scopedRule = scopedRule.replace(Style.regex.scope, "$1& $2");
         }
         scopedRule = `${scope} { ${scopedRule} }`;
-        console.log(scopedRule);
         return this.insert(scopedRule);
     }
 }
